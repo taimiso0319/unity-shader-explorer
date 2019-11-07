c986d7c9671b281313705f6575caba237150bedb
k-yasuda <k-yasuda@colopl.co.jp>
2019-11-07 16:16:19 +0900

    flutterプラグインからunity_shaders_explorerを切り出し

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/taimiso0319/unity-shader-explorer
+
+go 1.13
---
+++ standalone/main.go
@@ -0,0 +1,14 @@
+//
+// Created by taimiso0319 on 2019/11/07.
+// Copyright (c) 2019 taimiso0319. All rights reserved.
+//
+
+package main
+
+import (
+    "github.com/taimiso0319/unity_shaders_explorer/go/shaders"
+)
+
+func main(){
+    collect.Test();
+}
