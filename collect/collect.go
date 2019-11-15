d98f16691a6f04adad8d6b5fa83b89dfde47e389
k-yasuda <k-yasuda@colopl.co.jp>
2019-11-15 13:24:40 +0900

    Make walk directories functions and implements command-line params for it

--- collect/collect.go
+++ collect/collect.go
@@ -7,8 +7,51 @@ package collect
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
+var depthLimit int = 5
+var currentDepth int = 0
+
+// Test shows useless words as a first step.
 func Test() {
 	fmt.Print("test")
+}
+
+// WalkPath explorer directries recusively from the root path.
+func WalkPath(root string) {
+	var paths []string = dirwalk(root)
+	fmt.Println(paths)
+	fmt.Println(len(paths))
+}
+
+// SetDepthLimit changes the limit of directries the WalkPath explorers.
+func SetDepthLimit(depth int) {
+	depthLimit = depth
+}
+
+func dirwalk(dir string) []string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	var paths []string
+	for _, file := range files {
+		if file.IsDir() {
+			currentDepth++
+			if currentDepth > depthLimit {
+				fmt.Println("Too deep. To avoid this error if necessarily, please run the change limit method first.")
+				os.Exit(0) // I think I should change exit code to notice what happends.
+			}
+			paths = append(paths, dirwalk(filepath.Join(dir, file.Name()))...)
+			currentDepth--
+			continue
+		}
+		paths = append(paths, filepath.Join(dir, file.Name()))
+	}
+
+	return paths
 }
--- standalone/main.go
+++ standalone/main.go
@@ -6,9 +6,44 @@
 package main
 
 import (
+	"os"
+	//"flag"
+	"flag"
+	"fmt"
+
 	"github.com/taimiso0319/unity-shader-explorer/collect"
 )
 
 func main() {
-	collect.Test()
+	// Start setting flags
+	var (
+		limit int
+	)
+
+	f := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	f.IntVar(&limit, "limit", 5, "Max depth for limit explorering.")
+	f.Parse(os.Args[1:])
+	for 0 < f.NArg() {
+		f.Parse(f.Args()[1:])
+	}
+	if limit <= 0 {
+		fmt.Printf("limit can not be less than 0\n")
+		return
+	}
+	// End setting flags
+
+	if len(os.Args) <= 1 {
+		fmt.Println("You have to set the path start walk from.")
+		return
+	}
+	if _, err := os.Stat(os.Args[1]); os.IsNotExist(err) {
+		// path does not exist
+		fmt.Printf("%s does not exist. Please make sure the path exists.\n", os.Args[1])
+		return
+	}
+
+	fmt.Println(limit)
+	collect.SetDepthLimit(limit)
+
+	collect.WalkPath(os.Args[1])
 }
