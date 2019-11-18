6aa0ae6e9604139b268115d8bd402d94251529a9
k-yasuda <k-yasuda@colopl.co.jp>
2019-11-18 18:00:10 +0900

    testing method channel

--- plugin.go
+++ plugin.go
@@ -3,6 +3,8 @@ package shdrexpl
 import (
 	flutter "github.com/go-flutter-desktop/go-flutter"
 	"github.com/go-flutter-desktop/go-flutter/plugin"
+	"github.com/taimiso0319/unity-shader-explorer/analyze"
+	"github.com/taimiso0319/unity-shader-explorer/collect"
 )
 
 const channelName = "shader_explorer"
@@ -16,9 +18,15 @@ var _ flutter.Plugin = &UnityShaderExplorerPlugin{} // compile-time type check
 func (p *UnityShaderExplorerPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
 	channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
 	channel.HandleFunc("getPlatformVersion", p.handlePlatformVersion)
+	channel.HandleFunc("getShaders", p.handleCollectShader)
 	return nil
 }
 
 func (p *UnityShaderExplorerPlugin) handlePlatformVersion(arguments interface{}) (reply interface{}, err error) {
 	return "go-flutter " + flutter.PlatformVersion, nil
+}
+
+func (p *UnityShaderExplorerPlugin) handleCollectShader(path string) (reply interface{}, err error) {
+	var shaderPaths []string = collect.GetShaderPaths(path)
+	return analyze.GetShaderDetails(shaderPaths)
 }
